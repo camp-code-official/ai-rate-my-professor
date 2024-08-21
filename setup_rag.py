ff9ee4888706159b3c2e208bbe491c1f9a4f9d67
Thompson Ngo <61449896+tngo2001@users.noreply.github.com>
2024-08-20 22:35:42 -0700

    Integrated langchain for loading rmp links

--- setup_rag.py
+++ setup_rag.py
@@ -1,7 +1,12 @@
 from dotenv import load_dotenv
 load_dotenv()
+from langchain.document_loaders import WebBaseLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_pinecone import PineconeVectorStore
+from langchain.embeddings import OpenAIEmbeddings
 from pinecone import Pinecone, ServerlessSpec
 from openai import OpenAI
+import tiktoken
 import os
 import json
 
@@ -9,16 +14,19 @@ import json
 pc = Pinecone(api_key=os.getenv("PINECONE_API_KEY"))
 
 # Create a Pinecone index
-pc.create_index(
-    name="rag",
-    dimension=1536,
-    metric="cosine",
-    spec=ServerlessSpec(cloud="aws", region="us-east-1"),
-)
+if "rag" not in pc.list_indexes().names():
+    pc.create_index(
+        name="rag",
+        dimension=1536,
+        metric="cosine",
+        spec=ServerlessSpec(cloud="aws", region="us-east-1"),
+    )
 
 # Load the review data
 data = json.load(open("reviews.json"))
 
+# Initialize the OpenAI client
+embeddings = OpenAIEmbeddings()
 processed_data = []
 client = OpenAI()
 
@@ -50,3 +58,29 @@ print(f"Upserted count: {upsert_response['upserted_count']}")
 
 # Print index statistics
 print(index.describe_index_stats())
+
+# Initialize the text splitter
+tokenizer = tiktoken.get_encoding('p50k_base')
+
+def tiktoken_len(text):
+    tokens = tokenizer.encode(
+        text,
+        disallowed_special=()
+    )
+    return len(tokens)
+
+text_splitter = RecursiveCharacterTextSplitter(
+    chunk_size=2000,
+    chunk_overlap=100,
+    length_function=tiktoken_len,
+    separators=["\n\n", "\n", " ", ""]
+)
+
+# Load in a professor's ratings
+id = "498702"
+loader = WebBaseLoader("https://www.ratemyprofessors.com/professor/" + id)
+rmp_data = loader.load()
+texts = text_splitter.split_documents(rmp_data)
+
+# Insert data into Pinecone
+vectorstore_from_texts = PineconeVectorStore.from_texts([f"Source: {t.metadata['source']}, Title: {t.metadata['title']} \n\nContent: {t.page_content}" for t in texts], embeddings, index_name="rag", namespace="rmp")
